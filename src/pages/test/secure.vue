f37c73b109369a84ebd6b5852aad9c57dc150890
alinex <info@alinex.de>
2018-03-07 22:05:48 +0100

    Add authenticate test methods

--- src/pages/test/secure.vue
+++ src/pages/test/secure.vue
@@ -9,6 +9,24 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  created () {
+    console.log('-----------------------')
+    this.$feathers.authenticate().then(() => {
+      console.log('show application page')
+    }).catch(() => {
+      console.log('show login page')
+    })
+    console.log('-----------------------')
+    this.$feathers.authenticate({
+      strategy: 'local',
+      email: 'info@alinex.de',
+      password: 'secret'
+    }).then(() => {
+      console.log('Logged in')
+    }).catch((e) => {
+      console.log('failed', e)
+    })
+  }
 }
 </script>
